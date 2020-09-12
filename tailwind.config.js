e6df09219c769ee12e9d37c463460a1982f6a41e
Rizky Puji Raharja <rizky.rsj@gmail.com>
2020-09-12 20:15:39 +0700

    Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {},
+  variants: {},
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './public/**/*.html',
+      './src/**/*.vue'
+    ]
+  }
+}
