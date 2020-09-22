555d584ed5495995d48ef48d2349380079441123
Rizky Puji Raharja <rizky.rsj@gmail.com>
2020-09-22 23:08:33 +0700

    Popup Map

--- src/components/DataCountries.vue
+++ src/components/DataCountries.vue
@@ -20,7 +20,6 @@
           </span>
         </p>
       </div>
-      <hr />
     </div>
   </div>
 </template>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -12,7 +12,7 @@ export default {
   name: "Map",
   props: {
     dataCountries: Array,
-    country: Object
+    country: Object,
   },
 
   setup(props) {
@@ -79,27 +79,50 @@ export default {
           (60 * Math.sqrt(country.data.confirmed.total)) /
           Math.sqrt(maxConfirmed);
 
-        minichartCountries[country.name] = L.minichart(
-          country.coordinate,
-          {
-            data: scoresDiff,
-            type: "pie",
-            colors: [
-              "rgb(248 159 28 / 0.5)",
-              "rgb(63 196 182 / 0.5)",
-              "rgb(231 29 54 / 0.5)",
-            ],
-            width: width <= 7 ? 7 : width,
-          }
-        );
+        minichartCountries[country.name] = L.minichart(country.coordinate, {
+          properties: country,
+          data: scoresDiff,
+          type: "pie",
+          colors: [
+            "rgb(248 159 28 / 0.5)",
+            "rgb(63 196 182 / 0.5)",
+            "rgb(231 29 54 / 0.5)",
+          ],
+          width: width <= 7 ? 7 : width,
+        });
         state.map.addLayer(minichartCountries[country.name]);
+
+        minichartCountries[[country.name]].on("click", function (e) {
+          setPopup(e.target.options.properties);
+        });
       });
     }
 
     function setCenter() {
-      if(props.country != 'all'){
-        state.map.flyTo(new L.LatLng(props.country.info.lat, props.country.info.long));
+      if (props.country != "all") {
+        state.map.flyTo(
+          new L.LatLng(props.country.info.lat, props.country.info.long)
+        );
       }
+    }
+
+    function setPopup(country) {
+      function numberWithDot(x) {
+          return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      }
+
+      L.popup()
+        .setLatLng(country.coordinate)
+        .setContent(`
+            <center><img class="w-24 h-auto rounded-sm mb-2" src="${country.flag}" /></center>
+            <p class="text-lg text-gray-700 leading-none font-semibold">${country.name}</p>
+            <div class="divide-y divide-gray-400 text-md">
+              <div class="py-2 text-warning">Confirmed : <b>${numberWithDot(country.data.confirmed.total)}</b></div>
+              <div class="py-2 text-success">Recovered : <b>${numberWithDot(country.data.recovered.total)}</b></div>
+              <div class="py-2 text-danger">Death : <b>${numberWithDot(country.data.death.total)}</b></div>
+            </div>
+        `)
+        .openOn(state.map);
     }
 
     return {
